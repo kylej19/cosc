a9973f4c6082f173e7d0cb86a735b5d974a4f6a1
kyle <kj@qebian>
2025-10-20 02:36:01 +0000

    finished

--- build/5a/main.cpp
+++ build/5a/main.cpp
@@ -8,39 +8,54 @@
 
 int main()
 {
-  //generate a random number between 1-50 for the computers choice
-  
+  //create and seed a random number generator for the cpu selection  
   std::random_device rd;
   std::mt19937 gen(rd());
-  std::uniform_int_distribution<> distrib(1, 50);
+  //a flag for replaying the game
+  bool play_again {true};
+  char user_choice;
+  //variables for storing user guess and keeping track of attempts (as well as the lowest score of games)
+  int user_guess, computer_choice, upper_bound, score, best_score = 99;
 
-  int const COMPUTER_VAL = distrib(gen);
-  int user_guess, attempts = 0;
-  //std::cout << COMPUTER_VAL << std::endl;
+  std::cout << "Welcome to the Guessing Game!\n\nI will pick a number between 1 and any number you choose. Pick the upper bound: ";
+  std::cin >> upper_bound;
+  std::uniform_int_distribution<> distrib(1, upper_bound);
 
-  std::cout << "Welcome to the Guessing Game.\n\nI am picking a number between 1 and 50.\n\nTry to guess the number:";
-  std::cin >> user_guess;
-
-  user_guess = static_cast<int>(user_guess);
-
-  while(user_guess != COMPUTER_VAL)
+  
+  while(play_again)
     {
-      std::cout << "\n\n[x]\tIncorrect guess.\n\n";
-      if(user_guess > COMPUTER_VAL)
-	{
-	  std::cout << "(Your guess was too high).\n";
-	} else
-	{
-	  std::cout << "(Your guess was too low).\n";
-	}
-      std::cout << "Guess again: ";
-      attempts++;
+      score = 1;
+      computer_choice = distrib(gen);      
+      std::cout << "\nI am thinking of a number! What is it? ";
       std::cin >> user_guess;
+      while(user_guess != computer_choice)
+	{
+	  std::cout << "\n[x]\tIncorrect guess.\n";
+	  if(user_guess > computer_choice)
+	    {
+	      std::cout << "(Your guess was too high).\n";
+	    } else
+	    {
+	      std::cout << "(Your guess was too low).\n";
+	    }
+	  std::cout << "Guess again: ";
+	  score++;
+	  std::cin >> user_guess;
+	}
+      std::cout << "\nYou are correct! The number was " << computer_choice
+		<< "\nIt took you " << score << " attempts.\n";
+      
+      if(score < best_score)
+	{
+	  std::cout << "***You set a new best score! " << score << " attempts.***\n";
+	  best_score = score;
+	}
+      
+      std::cout << "Play again? [Y/n] ";
+      std::cin >> user_choice;
+      
+      if(user_choice == 'N' || user_choice == 'n') play_again = false;
     }
-  std::cout << "You are correct! The number was " << COMPUTER_VAL
-	    << "\n\nIt took you " << attempts << " attempts.\n";
-
-
-
+  
   std::cout << std::endl;
 }
