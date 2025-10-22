a6b0833e395cfc535470b2217ffcc7222ed13d50
kylej19 <kmj19@tuta.io>
2025-10-22 14:56:55 +0000

    new

--- daily/wed/oct22/main.cpp
+++ daily/wed/oct22/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <random>
+#include <string>
 
 int main()
 {
@@ -9,10 +10,27 @@ int main()
   std::uniform_int_distribution<int> distrib(1,100);
 
   int computer_val = distrib(gen);
-  std::cout << computer_val << '\n';
+  //std::cout << computer_val << '\n';
 
   // class exercises ...
+  std::string user_input {};
+  std::string vowells {"aeiou"};
+  std::cout << "Enter three or more words: ";
+  std::getline(std::cin, user_input);
 
-  
+  int count {0};
+    
+  for(int i = 0;i < user_input.length();i++)
+    {
+      for(int j = 0;j < vowells.length();j++)
+	{
+	  if(user_input[i] == vowells[j])
+	    {
+	      std::cout << "\nindex " << i << "/" << user_input.length()-1 << ": " << user_input[i];
+	      count++;
+	    }
+	}
+    }
+  std::cout << '\n' << count;
   std::cout << std::endl;
 }
