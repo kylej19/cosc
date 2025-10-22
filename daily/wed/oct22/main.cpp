c247851ad6a5794fbf15e3522d934c39c01cfb4a
kylej19 <kmj19@tuta.io>
2025-10-22 15:19:37 +0000

    new

--- daily/wed/oct22/main.cpp
+++ daily/wed/oct22/main.cpp
@@ -15,22 +15,34 @@ int main()
   // class exercises ...
   std::string user_input {};
   std::string vowells {"aeiou"};
+  
   std::cout << "Enter three or more words: ";
   std::getline(std::cin, user_input);
+  std::cout << "\n\n---\n\n";
 
-  int count {0};
+  char spaces = ' ';
+  int count_v = 0, count_space = 0;
+  
     
   for(int i = 0;i < user_input.length();i++)
     {
+      if(user_input[i] == spaces)
+	{
+	  std::cout << "\n";
+	  count_space++;
+	}
+      else std::cout << user_input[i];
       for(int j = 0;j < vowells.length();j++)
 	{
 	  if(user_input[i] == vowells[j])
 	    {
-	      std::cout << "\nindex " << i << "/" << user_input.length()-1 << ": " << user_input[i];
-	      count++;
+	      //std::cout << "\nindex " << i << "/" << user_input.length()-1 << ": " << user_input[i];
+	      count_v++;
 	    }
 	}
     }
-  std::cout << '\n' << count;
-  std::cout << std::endl;
+  std::cout << "\n\n---\tTOTALS\t---\n\n"
+	    << "\nwords: " << count_space + 1
+	    << "\nvowells: " << count_v 
+	    << "\n\n\n" << std::endl;
 }
