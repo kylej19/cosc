76e422f38bf5605c64851a3cb4c5809639b624e6
kylej19 <kmj19@tuta.io>
2025-12-02 01:40:01 -0600

    polished-output

--- proj/converter/main.cpp
+++ proj/converter/main.cpp
@@ -12,15 +12,18 @@ int main()
   int imperial_ft, imperial_in, total_in;
   double total_cm, metric_m;
 
-  std::cout << "Input (feet, inches): ";
+  // ask user for input and assign it to our variables
+  std::cout << "Input height (feet, inches): ";
   std::cin >> imperial_ft >> imperial_in;
 
+  // convert the height(feet, inches) into inches
   total_in = (imperial_ft * FEET_2_IN) + imperial_in;
-  
+  // convert inches into centimeters
   total_cm = total_in * IN_2_CM;
-
+  // convert centimeters into meters
   metric_m = total_cm * CM_2_M;
 
-  std::cout << std::setprecision(2) << "\n" << metric_m << std::endl;
+  // output height in meters
+  std::cout << std::setprecision(3) << "You are " << metric_m << " meters tall." <<std::endl;
   
 }
