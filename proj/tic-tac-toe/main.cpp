563650b38e6d9c928711bb03c9f5c5781d192e19
kylej19 <kmj19@tuta.io>
2025-12-16 22:34:18 -0600

    display

--- proj/tic-tac-toe/main.cpp
+++ proj/tic-tac-toe/main.cpp
@@ -11,35 +11,41 @@ int main()
   const unsigned short SIZE = 3;
   char playerChar, computerChar;
 
-  char board[SIZE][SIZE] =
-    {
-      {' ', ' ', ' '},
-      {' ', ' ', ' '},
-      {' ', ' ', ' '}
-    };
+  char board[SIZE][SIZE];
+
+
+  for(int row = 0; row < SIZE; row++) {
+    for(int col = 0; col < SIZE; col++) {
+      board[row][col] = ' ';
+    } }
 
 
   std::cout << "welcome to tic-tac-toe"
 	    << "\n"
-	    << "\n";
-  std::cout << "which letter do you want to be? [X/O]\n";
-  std::cin >> playerChar;
+	    << "please choose which letter you\n";
+  std::cout << "would like to play as\n";
+  while(!(playerChar == 'x' || playerChar == 'o')) {
+    std::cout << "enter selection [x/o]: ";
+    std::cin >> playerChar;
+  }
 
   switch(playerChar)
     {
-    case 'X' :
-      computerChar = 'O';
+    case 'x' :
+      computerChar = 'o';
       break;
-    case 'O' :
-      computerChar = 'X';
+    case 'o' :
+      computerChar = 'x';
       break;
     }
   
   std::cout << "your choice was " << playerChar << "\nthe computer will be " << computerChar << std::endl;
-  
+
+  std::cout << "use the map to choose your first move: \n";
+  char map = '1';  
   for(int i = 0; i < SIZE; i++) {
     for(int j = 0; j < SIZE; j++) {
-      std::cout << board[i][j] << ((j + 1) % SIZE ? '|' : '\n');
+      std::cout << map++ << ((j + 1) % SIZE ? '|' : '\n');
     } }  
 }
 
